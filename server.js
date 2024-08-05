509ee156c7b1a009e3e091f429dc374e72892731
ainiazzah <aini.21006@mhs.unesa.ac.id>
2024-08-05 21:51:57 +0700

    update: creating http server using hapi

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Starting project web service using hapi');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+
+    await server.start();
+    console.log(`Server running on ${server.info.uri}`);
+}
+
+init();
