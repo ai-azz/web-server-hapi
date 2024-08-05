4601eb8d5667eb1c22756f35287de102bfdbd237
ainiazzah <aini.21006@mhs.unesa.ac.id>
2024-08-05 22:05:19 +0700

    add: comments on server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+// import the Hapi module
 const Hapi = require('@hapi/hapi');
 
+// create asynchronous func to configure and start the server
 const init = async () => {
+    // create a new Hapi server instance
     const server = Hapi.server({
         port: 5000,
         host: 'localhost',
     });
 
-    await server.start();
-    console.log(`Server running on ${server.info.uri}`);
+    await server.start();  // start the server
+    console.log(`Server running on ${server.info.uri}`);  // log the server URI
 }
 
-init();
+init(); // initialize the server
