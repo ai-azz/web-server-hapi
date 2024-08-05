e04ddf02899857b9a7464a3dccf14c8ea0f2ebd4
ainiazzah <aini.21006@mhs.unesa.ac.id>
2024-08-05 22:14:07 +0700

    add: routing

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const init = async () => {
         host: 'localhost',
     });
 
+    // create routing on hapi project (using route configuration object on `server.route()` method)
+    server.route({
+        method: 'GET',  // specify the HTTP method for the route
+        path: '/',  // define the URL path for the route
+        handler: (request, h) => {
+            // response to be sent when the route is accessed
+            return 'Hello World!';
+        }
+    });
+
     await server.start();  // start the server
     console.log(`Server running on ${server.info.uri}`);  // log the server URI
 }
