0e091e3c2f30aecb42b166fa276ec67a353f11ca
ainiazzah <aini.21006@mhs.unesa.ac.id>
2024-08-06 11:53:17 +0700

    update: routes.js add set of routes

--- routes.js
+++ routes.js
@@ -1,21 +1,44 @@
+// define an array of route objects for configuring server routes
 const routes = [
-    {           
-        method: 'GET',  // the HTTP method for the 1st route
-        path: '/',  // the URL path for the  1st route
+    {   // root path
+        method: 'GET',  // http method for the root path (GET request)
+        path: '/',  // the url path for the root path
         handler: (request, h) => {
-            // response returned for requests to the root path
-            return 'Hello World!';
+            // response returned for the GET request to the root path
+            return 'Homepage';
         },
     },
-    {          
-        method: 'GET',  // the http method for the 2nd route
-        path: '/about',  // the URL path for the 2nd route
+    {   // root path
+        method: '*', // the wildcard method to match all http methods
+        path: '/',  // url path for the root path
         handler: (request, h) => {
-            // response returned for request to the /about path
+            // response returned for the non-GET requests to the root path
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {   // /about path
+        method: 'GET',  // http method for about path (GET request)
+        path: '/about',  // the url path for about path
+        handler: (request, h) => {
+            // response returned for GET request to the /about path
             return 'About page';
+        },
+    },
+    {   // /about path
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman about tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {   // /{any*} path -> catch all path or any path that doesn't match the previous defined routes
+        method: '*',  // the wildcard method to match all http methods
+        path: '/{any*}',  // a wildcard path to match any path not defined above
+        handler: (request, h) => {
+            // response returned for any path that doesn't match defined routes
+            return 'Halaman tidak dapat ditemukan';
         },
     },
 ];
 
-// export the routes array to be imported and used in server.js file
 module.exports = routes;
